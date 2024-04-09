3dec7b517d50c927f742744d19e96696b84120b4
Anshul Dalal <anshulusr@gmail.com>
2024-04-09 16:32:03 +0530

    engine: evaluate score based on piece count

--- src/engine.rs
+++ src/engine.rs
@@ -1,7 +1,22 @@
-use chess::{Board, ChessMove, MoveGen};
-use macroquad::rand::rand;
+use chess::{Board, ChessMove, Color, MoveGen, Square};
 
 pub fn next_move(board: &Board) -> Option<ChessMove> {
-    let mut movegen = MoveGen::new_legal(&board);
-    movegen.nth(rand() as usize % movegen.len())
+    let movegen = MoveGen::new_legal(&board);
+    movegen
+        .map(|mov| {
+            let score = evaluate(&board.make_move_new(mov));
+            (score, mov)
+        })
+        .max_by_key(|(score, _mov)| *score)
+        .map(|(_, mov)| mov)
+}
+
+fn evaluate(board: &Board) -> isize {
+    (0..64)
+        .map(|i| match unsafe { board.color_on(Square::new(i)) } {
+            None => 0,
+            Some(Color::White) => -1,
+            Some(Color::Black) => 1,
+        })
+        .sum()
 }
