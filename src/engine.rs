018f5f9de76d04b9a28857f9124806452bdfb725
Anshul Dalal <anshulusr@gmail.com>
2024-04-09 16:56:40 +0530

    engine: add piece weights

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,4 @@
-use chess::{Board, ChessMove, Color, MoveGen, Square};
+use chess::{Board, ChessMove, Color, MoveGen, Piece, Square};
 
 pub fn next_move(board: &Board) -> Option<ChessMove> {
     let movegen = MoveGen::new_legal(&board);
@@ -11,12 +11,32 @@ pub fn next_move(board: &Board) -> Option<ChessMove> {
         .map(|(_, mov)| mov)
 }
 
+const fn piece_weight(piece: Piece) -> isize {
+    match piece {
+        Piece::Pawn => 1,
+        Piece::Knight => 3,
+        Piece::Bishop => 3,
+        Piece::Rook => 5,
+        Piece::Queen => 9,
+        Piece::King => 20,
+    }
+}
+
 fn evaluate(board: &Board) -> isize {
     (0..64)
-        .map(|i| match unsafe { board.color_on(Square::new(i)) } {
-            None => 0,
-            Some(Color::White) => -1,
-            Some(Color::Black) => 1,
+        .map(|i| unsafe { Square::new(i) })
+        .map(|i| {
+            board
+                .color_on(i)
+                .map(|color| match color {
+                    Color::White => -1,
+                    Color::Black => 1,
+                })
+                .unwrap_or(0)
+                * board
+                    .piece_on(i)
+                    .map(|piece| piece_weight(piece))
+                    .unwrap_or(0)
         })
         .sum()
 }
